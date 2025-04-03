0c6c3bd48459c2481cde57b0bd2139a348d507cc
Keplin1 <irin.keplin@gmail.com>
2025-04-03 13:53:35 +0100

    update to sortAndOrderArticles model

--- model.js
+++ model.js
@@ -105,7 +105,21 @@ const sortAndOrderArticles = (sortValue = 'created_at', orderValue = 'desc', lim
     if (!allowedInputSort.includes(sortValue) || !allowedInputsOrder.includes(orderValue)) {
         return Promise.reject({ status: 400, message: "Invalid sort or order value" });
     } else {
-        return db.query(`SELECT * FROM articles ORDER BY ${sortValue} ${orderValue} LIMIT $1 OFFSET $2`, [limit, offSet]).then(({ rows }) => {
+        return db.query(`            
+            SELECT articles.author, 
+            articles.title, 
+            articles.article_id, 
+            articles.topic, 
+            articles.created_at, 
+            articles.votes, 
+            articles.article_img_url,  
+            COUNT(comments.comment_id)::INT AS comment_count 
+            FROM articles 
+            LEFT JOIN comments 
+                ON articles.article_id = comments.article_id 
+            GROUP BY articles.article_id 
+            ORDER BY ${sortValue} ${orderValue} 
+            LIMIT $1 OFFSET $2`, [limit, offSet]).then(({ rows }) => {
             return rows
         })
     }
